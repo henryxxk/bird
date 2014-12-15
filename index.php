e1f53a9d127a04b7e3d08a2f2e2744e6d3f10a5b
henry <46354856@qq.com>
2014-12-15 09:58:24 +0800

    commit 2014.12.14 9:58

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-require "../lib/common.php";
+require_once dirname(__FILE__)."/lib/common.php";
+require_once dirname(__FILE__)."/class/token.php";
+
+
 $token = new Token($_Globals['Appid'], $_Globals['Secret']);
 
 $openid = $token->getTokenOpenId();
